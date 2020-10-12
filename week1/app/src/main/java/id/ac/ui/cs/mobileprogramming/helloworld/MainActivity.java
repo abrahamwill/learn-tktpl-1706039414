6d88a53d85c4f67ea9ce53e34395a1977b645b44
abrahamwill <abraham.williams@ui.ac.id>
2020-10-12 12:18:15 +0700

    lab-4

--- week1/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ week1/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -1,16 +1,19 @@
 package id.ac.ui.cs.mobileprogramming.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import id.ac.ui.cs.mobileprogramming.helloworld.R;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TaskListFragment taskListFragment = new TaskListFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, taskListFragment)
+                .addToBackStack("task_list")
+                .commit();
     }
 }
