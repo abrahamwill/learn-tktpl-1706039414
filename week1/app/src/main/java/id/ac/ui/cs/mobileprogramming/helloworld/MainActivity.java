84baefac3a37179b81f83bc333ed5bc52ceff6ff
abrahamwill <abraham.williams@ui.ac.id>
2020-12-14 15:25:06 +0700

    Lab-7 Done, create simple counter app

--- week1/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ week1/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -3,14 +3,36 @@ package id.ac.ui.cs.mobileprogramming.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.w3c.dom.Text;
 
 import id.ac.ui.cs.mobileprogramming.helloworld.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int counter;
+    private TextView countertxt;
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        countertxt = (TextView) findViewById(R.id.counter);
+        fab = (FloatingActionButton) findViewById(R.id.button_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                countertxt.setText(String.valueOf(counter));
+            }
+        });
+
     }
 }
